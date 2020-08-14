cb1002ad48b2526273f9f21c5eec1c80bfbc24f6
Allan Ortiz <aortez@gmail.com>
2020-08-14 03:49:39 -0700

    flip between text and images

--- write_to_lcd.cs
+++ write_to_lcd.cs
@@ -3,26 +3,51 @@ public Program()
     Runtime.UpdateFrequency = UpdateFrequency.Update10;
 }
 
+List<String> imageList = new List<String>();
+
+TimeSpan flipPeriod = new TimeSpan(0, 0, 5);
+TimeSpan timeSinceLastFlip = new TimeSpan(0, 0, 0);
+
+bool shouldShowText = false;
+
 public void Main(string argument, UpdateType updateType)
 {
+    timeSinceLastFlip += Runtime.TimeSinceLastRun;
+    Echo ("s: " + Runtime.LastRunTimeMs + ", timeSinceLastFlip: " + timeSinceLastFlip);
+    if (timeSinceLastFlip > flipPeriod) {
+        shouldShowText = !shouldShowText;
+        timeSinceLastFlip = new TimeSpan(0, 0, 0);
+    }
+
     IMyTextSurfaceProvider lcdButtonPanel = GridTerminalSystem.GetBlockWithName("The Button") as IMyTextSurfaceProvider;
     if (lcdButtonPanel == null) {
         Echo ("couldn't find the button panel");
         return;
     }
 
-    Echo ("found the button panel: " + lcdButtonPanel.ToString());
-
     IMyTextSurface drawingSurface = lcdButtonPanel.GetSurface(0);
-
     if (drawingSurface == null) {
         Echo ("no drawing surface on the block?");
         return;
     }
 
-    Echo ("I have something to draw on");
+    // Time to show the text.
+    if (shouldShowText) {
+        // If it is showing an image currently, copy the image ID for later
+        // so we can switch back to showing it.
+        if (!String.IsNullOrEmpty(drawingSurface.CurrentlyShownImage)) {
+            drawingSurface.GetSelectedImages(imageList);
 
-    drawingSurface.ContentType = ContentType.TEXT_AND_IMAGE;
-    drawingSurface.Alignment = VRage.Game.GUI.TextPanel.TextAlignment.CENTER;
-    drawingSurface.WriteText("hey it works!");
+            // After we have the list of images, clear them so we can see our output
+            // text.
+            drawingSurface.ClearImagesFromSelection();
+        }
+        drawingSurface.ContentType = ContentType.TEXT_AND_IMAGE;
+        drawingSurface.Alignment = VRage.Game.GUI.TextPanel.TextAlignment.CENTER;
+        drawingSurface.WriteText("showing text");
+    } else {
+        // Show whatever was there previously...
+        drawingSurface.WriteText("");
+        drawingSurface.AddImagesToSelection(imageList, true);
+    }
 }
