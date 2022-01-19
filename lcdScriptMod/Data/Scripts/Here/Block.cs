e6cd5f989f73f60650af80b8d666dd2984e72574
Allan Ortiz <aortez@gmail.com>
2022-01-18 22:09:21 -0800

    Add WIP LCD script mod

---
+++ lcdScriptMod/Data/Scripts/Here/Block.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using System.Collections.Generic;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Game.ModAPI.Ingame;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+
+using Digi;
+
+using IMyCubeBlock = VRage.Game.ModAPI.Ingame.IMyCubeBlock;
+using IMyCubeGrid = VRage.Game.ModAPI.IMyCubeGrid;
+using IMyEntity = VRage.Game.ModAPI.Ingame.IMyEntity;
+using IMyInventory = VRage.Game.ModAPI.Ingame.IMyInventory;
+using IMySlimBlock = VRage.Game.ModAPI.IMySlimBlock;
+
+/*
+Cache is held by the LC
+Cache probes are done against all other blocks.
+
+*/
+
+namespace AllanTTS
+{
+
+public class StuffCache {
+    public StringBuilder IngotsMessage = new StringBuilder();
+    public StringBuilder OresMessage = new StringBuilder();
+
+    // Ore Item types.
+    private List<Item> ores = new List<Item>() {
+        new Item("Cobalt", "Cobalt", "MyObjectBuilder_Ore"),
+        new Item("Gold", "Gold", "MyObjectBuilder_Ore"),
+        new Item("Ice", "Ice", "MyObjectBuilder_Ore"),
+        new Item("Iron", "Iron", "MyObjectBuilder_Ore"),
+        new Item("Magnesium", "Magnesium", "MyObjectBuilder_Ore"),
+        new Item("Nickel", "Nickel", "MyObjectBuilder_Ore"),
+        new Item("Platinum", "Platinum", "MyObjectBuilder_Ore"),
+        new Item("Scrap", "Scrap", "MyObjectBuilder_Ore"),
+        new Item("Silicon", "Silicon", "MyObjectBuilder_Ore"),
+        new Item("Silver", "Silver", "MyObjectBuilder_Ore"),
+        new Item("Stone", "Stone", "MyObjectBuilder_Ore"),
+        new Item("Uranium", "Uranium", "MyObjectBuilder_Ore")
+    };
+
+    // Ore Item types.
+    private List<Item> ingots = new List<Item>() {
+        new Item("Cobalt", "Cobalt", "MyObjectBuilder_Ingot"),
+        new Item("Gold", "Gold", "MyObjectBuilder_Ingot"),
+        new Item("Gravel", "Stone", "MyObjectBuilder_Ingot"),
+        new Item("Iron", "Iron", "MyObjectBuilder_Ingot"),
+        new Item("Magnesium", "Magnesium", "MyObjectBuilder_Ingot"),
+        new Item("Nickel", "Nickel", "MyObjectBuilder_Ingot"),
+        new Item("Platinum", "Platinum", "MyObjectBuilder_Ingot"),
+        new Item("Silicon", "Silicon", "MyObjectBuilder_Ingot"),
+        new Item("Silver", "Silver", "MyObjectBuilder_Ingot"),
+        new Item("Uranium", "Uranium", "MyObjectBuilder_Ingot")
+    };
+
+    // Blocks on this grid.
+    private List<IMySlimBlock> blocks = new List<IMySlimBlock>();
+
+    // Is the cache fresh? The holder of this cache needs to maintain this state
+    // if they want it to be valid.
+    public bool IsFresh;
+
+    public void RefreshCache(IMyCubeGrid grid) {
+        Log.Info("refreshing cache");
+
+        // Fetch all blocks in grid.
+        // IMyGridTerminalSystem gridTerminal = MyAPIGateway.TerminalActionsHelper.GetTerminalSystemForGrid((Sandbox.ModAPI.IMyCubeGrid)grid);
+        // ((Sandbox.ModAPI.IMyCubeGrid)grid).GetBlocks(blocks);
+        grid.GetBlocks(blocks);
+
+        // Check to see if they have what we're looking for.
+        foreach (IMySlimBlock slimBlock in blocks)
+        {
+            IMyCubeBlock block = slimBlock.FatBlock;
+            if (block == null || !block.HasInventory) {
+                 continue;
+            }
+
+            foreach (Item item in ingots)
+            {
+                item.Count += CountItemType(block, item.Type);
+            }
+
+            foreach (Item item in ores)
+            {
+                item.Count += CountItemType(block, item.Type);
+            }
+        }
+
+        OresMessage = new StringBuilder();
+        OresMessage.AppendLine("--- Ores ---");
+        foreach (Item item in ores)
+        {
+            MaybePrintCount(OresMessage, item);
+            item.Count = 0;
+        }
+
+        IngotsMessage = new StringBuilder();
+        IngotsMessage.AppendLine("--- Ingots ---");
+        foreach (Item item in ingots)
+        {
+            MaybePrintCount(IngotsMessage, item);
+            item.Count = 0;
+        }
+    }
+
+    private void MaybePrintCount(StringBuilder stringBuilder, Item item)
+    {
+        if (item.Count <= 0) {
+            return;
+        }
+
+        stringBuilder.AppendLine(item.DisplayName + ": " + RawAmountToWeight(item.Count));
+    }
+
+    private long CountItemType(IMyEntity block, MyItemType type)
+    {
+        long count = 0;
+
+        if (!block.HasInventory)
+        {
+            return count;
+        }
+
+        for (int i = 0; i < block.InventoryCount; i++)
+        {
+            IMyInventory inventory = block.GetInventory(i);
+            count += (long)inventory.GetItemAmount(type);
+        }
+        return count;
+    }
+
+    private string RawAmountToWeight(long rawAmount)
+    {
+        if (rawAmount < 1000) {
+            return (rawAmount / 1.0f).ToString("#,#0.0") + " kg";
+        } else if (rawAmount < 1000000) {
+            return (rawAmount / 1000.0f).ToString("#,#0.0") + " k";
+        } else {
+            return (rawAmount / 1000000.0f).ToString("#,#0.0") + " Gg";
+        }
+    }
+}
+
+// change MyObjectBuilder_BatteryBlock to the block type you're using, it must be the exact type, no inheritence.
+[MyEntityComponentDescriptor(typeof(MyObjectBuilder_TextPanel), false, "AllanLargeLCDPanel")]
+public class ProtectionBlock : MyGameLogicComponent
+{
+    public StuffCache Cache = new StuffCache();
+
+    public bool IsOptionSet;
+
+    private IMyFunctionalBlock block;
+
+    private bool createdTerminalControls;
+
+    // this method is called async! always do stuff in the first update unless you're sure it must be in Init().
+    public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+    {
+        NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        Cache.IsFresh = false;
+        Log.Info("ProtectionBlock.Init", "ProtectionBlock.Init", 500);
+    }
+
+    public override void UpdateOnceBeforeFrame() // first update of the block
+    {
+        block = (IMyFunctionalBlock)Entity;
+
+        if (block.CubeGrid?.Physics == null) // ignore projected and other non-physical grids
+            return;
+
+        ProtectionSession.Instance?.ProtectionBlocks.Add(block);
+
+        CreateTerminalControls();
+
+        Log.Info("UpdateOnceBeforeFrame", "UpdateOnceBeforeFrame", 500);
+    }
+
+    public override void Close() // called when block is removed for whatever reason (including ship despawn)
+    {
+        ProtectionSession.Instance?.ProtectionBlocks.Remove((IMyFunctionalBlock)Entity);
+    }
+
+    public override void UpdateBeforeSimulation()
+    {
+        if (!block.IsFunctional)
+        {
+            return;
+        }
+
+        Log.Info("UpdateBeforeSimulation", "UpdateBeforeSimulation", 500);
+    }
+
+    private static void TerminalControls_CustomControlGetter(IMyTerminalBlock b, List<IMyTerminalControl> controls)
+    {
+        Log.Info("\nCandidate DefinitionDisplayNameText: " + b.DefinitionDisplayNameText);
+        Log.Info("Candidate DetailedInfo: " + b.DetailedInfo);
+        Log.Info("Candidate DisplayName: " + b.DisplayName);
+        Log.Info("Candidate EntityId: " + b.EntityId);
+        Log.Info("Candidate Name: " + b.Name);
+
+        // if (!(b is IMyTextPanel)) {
+        if (b.DefinitionDisplayNameText != "LCD Panel Large (Allan)") {
+            return;
+        }
+
+        try {
+            // Add a checkbox.  It doesn't do anything yet.
+            var checkbox = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlCheckbox, IMyTextPanel>("DO somethign");
+            checkbox.Title = MyStringId.GetOrCompute("Yeet");
+            checkbox.Tooltip = MyStringId.GetOrCompute("You guessed it.\nNope, actually try again.");
+            checkbox.Getter = (block) =>
+            {
+                if (block == null || block.GameLogic == null) return false;
+                Log.Info("checkbox.Getter");
+
+                var logic = block.GameLogic.GetAs<ProtectionBlock>();
+                bool isSet = logic != null ? logic.IsOptionSet : false;
+                return isSet;
+            };
+
+            checkbox.Setter = (block, value) =>
+            {
+                Log.Info("checkbox.Setter: setting value to: " + value);
+
+                var logic = block.GameLogic.GetAs<ProtectionBlock>();
+
+                if (logic == null) return;
+
+                logic.IsOptionSet = value;
+            };
+
+            controls.Add(checkbox);
+
+            Log.Info("Added checkbox somewhere", "Added checkbox somewhere", 500);
+
+            // Add a slider, hopefully.
+            IMyTerminalControlSlider slider = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlSlider, IMyTextPanel>("Sliderry");
+            slider.Title = MyStringId.GetOrCompute("sleet");
+            slider.Tooltip = MyStringId.GetOrCompute("slip from side to side");
+            // SpeedMultiplier.SupportsMultipleBlocks = true;
+            // SpeedMultiplier.Enabled = HasBlockLogic;
+            // SpeedMultiplier.Visible = HasBlockLogic;
+            slider.SetLimits(0.5f, 2.0f);
+            slider.Getter = (block) =>
+            {
+                Log.Info("slider.Getter");
+                return 0.5f;
+            };
+
+            slider.Setter = (block, value) =>
+            {
+                Log.Info("slider.Setter: " + value);
+            };
+
+            controls.Add(slider);
+            // SpeedMultiplier.Getter = (Block) => BlockReturn(Block, x => x.SpeedMultiplier);
+   // SpeedMultiplier.Setter = (Block, NewSpeed) => BlockAction(Block, x => x.SpeedMultiplier = (int)NewSpeed);
+   // SpeedMultiplier.Writer = (Block, Info) => Info.Append($"x{BlockReturn(Block, x => x.SpeedMultiplier)}");
+   // return SpeedMultiplier;
+
+        }
+        catch (Exception e) {
+            Log.Error(e);
+        }
+    }
+
+    void CreateTerminalControls() {
+        if(createdTerminalControls)
+            return;
+
+        Log.Info("CreateTerminalControls", "CreateTerminalControls", 500);
+
+        createdTerminalControls = true;
+
+        MyAPIGateway.TerminalControls.CustomControlGetter += TerminalControls_CustomControlGetter;
+    }
+}
+
+}
---
+++ lcdScriptMod/Data/Scripts/Here/OreTSS.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sandbox.Game.GameSystems.TextSurfaceScripts;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.Utils;
+using VRageMath;
+
+// Avoid including ingame namespaces because they cause ambiguity errors, instead, do aliases:
+using IMyGridTerminalSystem = Sandbox.ModAPI.Ingame.IMyGridTerminalSystem;
+using IMyTerminalBlock = Sandbox.ModAPI.Ingame.IMyTerminalBlock;
+
+using IMyInventory = VRage.Game.ModAPI.Ingame.IMyInventory;
+using MyItemType = VRage.Game.ModAPI.Ingame.MyItemType;
+
+using Digi;
+
+namespace AllanTTS
+{
+
+// Text Surface Scripts (TSS) can be selected in any LCD's scripts list.
+// These are meant as fast no-sync (sprites are not sent over network) display scripts, and the Run() method only executes player-side (no DS).
+// You can still use a session comp and access it through this to use for caches/shared data/etc.
+//
+// The display name has localization support aswell, same as a block's DisplayName in SBC.
+public class IngotsAndOresTSS : MyTSSCommon
+{
+    protected ItemGroups mode;
+
+    private StuffCache cache = new StuffCache();
+
+    // Update frequency, in number of frames.
+    public override ScriptUpdate NeedsUpdate => ScriptUpdate.Update100;
+
+    // The TerminalBlock that this TSS lives in.
+    private readonly Sandbox.ModAPI.IMyTerminalBlock TerminalBlock;
+
+    public IngotsAndOresTSS(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
+    {
+        mode = ItemGroups.IngotsAndOres;
+        // Called when script is created.
+        // This class is instanced per LCD that uses it, which means the same block can have multiple instances of this script aswell (e.g. a cockpit with all its screens set to use this script).
+        TerminalBlock = (Sandbox.ModAPI.IMyTerminalBlock)block; // internal stored m_block is the ingame interface which has no events, so can't unhook later on, therefore this field is required.
+        TerminalBlock.OnMarkForClose += BlockMarkedForClose; // required if you're gonna make use of Dispose() as it won't get called when block is removed or grid is cut/unloaded.
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose(); // do not remove
+        TerminalBlock.OnMarkForClose -= BlockMarkedForClose;
+
+        // Called when script is removed for any reason, so that you can clean up stuff if you need to.
+    }
+
+    void BlockMarkedForClose(IMyEntity ent)
+    {
+        Dispose();
+    }
+
+    // Gets called at the rate specified by NeedsUpdate.
+    // It can't run every tick because the LCD is capped at 6fps anyway.
+    public override void Run()
+    {
+        try
+        {
+            base.Run();
+
+            // If we have a parent block, let's see if it has an up to date cache that we can use.
+            // ProtectionBlock parentBlock = TerminalBlock.GameLogic.GetAs<ProtectionBlock>();
+            // if (parentBlock != null && parentBlock.IsCacheFresh == true) {
+            //     Log.Info("Use cache from parent");
+            //
+            //     // We'll just use the parent block's cache from now on out.
+            //     cache = parentBlock.Cache;
+            // }
+
+            // If cache is stale, then run an update.
+            // if (!cache.IsFresh) {
+            cache = new StuffCache();
+            cache.RefreshCache(TerminalBlock.CubeGrid);
+
+                // TODO: Figure out how to copy data from this block to other blocks
+                // of this type on this grid.
+                //
+
+                // Now that our cache is fresh, update the cache of the
+                // othe blocks in the grid.
+                // foreach (Sandbox.ModAPI.IMyTerminalBlock block in blocks) {
+                //     if (TerminalBlock.EntityId != block.EntityId) {
+                //         continue;
+                //     }
+                //     try {
+                //         // IngotsAndOresTSS other = (IngotsAndOresTSS) block;
+                //         // if (other != null) {
+                //         // block.ingotsMessage = ingotsMessage;
+                //
+                //             // other.ingotsMessage = ingotsMessage;
+                //             // other.oresMessage = oresMessage;
+                //         // MyAPIGateway.Utilities.ShowNotification("refreshed cache!!");
+                //         // }
+                //     }
+                //     catch(Exception e) {
+                //     }
+                // }
+            // } else {
+                // MyAPIGateway.Utilities.ShowNotification("using cache");
+            // }
+
+            // TODO: We don't want to duplicate work, so first check to see
+            // if we have an up to date answer already.  If not, then compute it
+            // and reach out to other similar blocks on this grid and share the
+            // result.
+
+            // Here's how to show a pop up notification:
+//                MyAPIGateway.Utilities.ShowNotification("num blocks: " + blocks.Count);
+
+            // Unless someone else refreshes our cache, then next time we wake up,
+            // we will do it ourself.
+            // cache.IsFresh = false;
+
+            // hold L key to see how the error is shown, remove this after you've played around with it =)
+            if (MyAPIGateway.Input.IsKeyPress(VRage.Input.MyKeys.L)) {
+                throw new Exception("Oh noes an error :}");
+            }
+
+            if (mode == ItemGroups.Ingots) {
+                Draw(cache.IngotsMessage.ToString());
+            }
+            else if (mode == ItemGroups.Ores) {
+                Draw(cache.OresMessage.ToString());
+            }
+            else if (mode == ItemGroups.IngotsAndOres) {
+                Draw(cache.OresMessage.ToString() + "\n" + cache.IngotsMessage.ToString());
+            }
+            else {
+                throw new Exception("Oh noes an error :(...");
+            }
+
+        }
+        catch(Exception e)
+        {
+            // Display the error to the user.
+            DrawError(e);
+        }
+    }
+
+    // Drawing sprites works exactly like in PB API.
+    // Therefore this guide applies: https://github.com/malware-dev/MDK-SE/wiki/Text-Panels-and-Drawing-Sprites
+
+    // there are also some helper methods from the MyTSSCommon that this extends.
+    // like: AddBackground(frame, Surface.ScriptBackgroundColor); - a grid-textured background
+
+    // the colors in the terminal are Surface.ScriptBackgroundColor and Surface.ScriptForegroundColor, the other ones without Script in name are for text/image mode.
+
+    void Draw(String message)
+    {
+        Vector2 screenSize = Surface.SurfaceSize;
+        Vector2 screenCorner = (Surface.TextureSize - screenSize) * 0.5f;
+
+        float fontScale = 1.0f;
+        float length = screenSize.Length();
+        if (length < 500.0f) {
+            fontScale = 0.5f;
+        }
+
+        var frame = Surface.DrawFrame();
+        var text = MySprite.CreateText(message, "Monospace", Surface.ScriptForegroundColor, fontScale, TextAlignment.LEFT);
+        text.Position = screenCorner + new Vector2(16, 16); // 16px from topleft corner of the visible surface
+        frame.Add(text);
+
+        // add more sprites and stuff
+
+        frame.Dispose(); // send sprites to the screen
+    }
+
+    // The original example Draw() method.
+    void Draw()
+    {
+        Vector2 screenSize = Surface.SurfaceSize;
+        Vector2 screenCorner = (Surface.TextureSize - screenSize) * 0.5f;
+
+        var frame = Surface.DrawFrame();
+        var text = MySprite.CreateText("Hi!", "Monospace", Surface.ScriptForegroundColor, 1f, TextAlignment.LEFT);
+        text.Position = screenCorner + new Vector2(16, 16); // 16px from topleft corner of the visible surface
+        frame.Add(text);
+
+        // add more sprites and stuff
+
+        frame.Dispose(); // send sprites to the screen
+    }
+
+    void DrawError(Exception e)
+    {
+        MyLog.Default.WriteLineAndConsole($"{e.Message}\n{e.StackTrace}");
+
+        try // first try printing the error on the LCD
+        {
+            Vector2 screenSize = Surface.SurfaceSize;
+            Vector2 screenCorner = (Surface.TextureSize - screenSize) * 0.5f;
+
+            var frame = Surface.DrawFrame();
+
+            var bg = new MySprite(SpriteType.TEXTURE, "SquareSimple", null, null, Color.Black);
+            frame.Add(bg);
+
+            var text = MySprite.CreateText($"ERROR: {e.Message}\n{e.StackTrace}\n\nPlease send screenshot of this to mod author.\n{MyAPIGateway.Utilities.GamePaths.ModScopeName}", "White", Color.Red, 0.7f, TextAlignment.LEFT);
+            text.Position = screenCorner + new Vector2(16, 16);
+            frame.Add(text);
+
+            frame.Dispose();
+        }
+        catch(Exception e2)
+        {
+            MyLog.Default.WriteLineAndConsole($"Also failed to draw error on screen: {e2.Message}\n{e2.StackTrace}");
+            if(MyAPIGateway.Session?.Player != null)
+                MyAPIGateway.Utilities.ShowNotification($"[ ERROR: {GetType().FullName}: {e.Message} | Send SpaceEngineers.Log to mod author ]", 10000, MyFontEnum.Red);
+        }
+    }
+}
+
+[MyTextSurfaceScript("Ores", "Ores")]
+public class Ores : IngotsAndOresTSS {
+    public Ores(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
+    {
+        base.mode = ItemGroups.Ores;
+        Log.Info("new Ores TSS created");
+    }
+}
+
+[MyTextSurfaceScript("IngotsAndOres", "IngotsAndOres")]
+public class IngotsAndOres : IngotsAndOresTSS {
+    public IngotsAndOres(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
+    {
+        base.mode = ItemGroups.IngotsAndOres;
+        Log.Info("new Ingots and Ores TSS created");
+    }
+}
+
+[MyTextSurfaceScript("Ingots", "Ingots")]
+public class Ingots : IngotsAndOresTSS {
+    public Ingots(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
+    {
+        base.mode = ItemGroups.Ingots;
+        Log.Info("new Ingots TSS created");
+    }
+}
+
+}
